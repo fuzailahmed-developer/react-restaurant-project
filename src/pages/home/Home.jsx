a84ba903771f8f7f89ae54cf7cb120e0967ab8cd
Fuzail Ahemd <fuzailcode@gmail.com>
2025-11-25 23:09:19 +0500

    new update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,17 @@
 import Cart from '@/components/my_com/Cart'
 import Items from '@/components/my_com/Items'
-import Menu from '@/components/my_com/menu'
+import Menu from '@/components/my_com/Menu'
 import Navbar from '@/components/my_com/Navbar'
 import React, { useState } from 'react'
 
-
 const Home = () => {
-
-  const [menu,setMenu] = useState(false);
-
+  const [menu, setMenu] = useState(false);
   return (
     <div className='min-h-screen bg-slate-200'>
-        <Navbar setMenu={setMenu} />
-        <Menu />
-        <Items />
-        <Cart setMenu={setMenu} menu={menu}/>
+      <Navbar setMenu={setMenu} />
+      <Menu />
+      <Items />
+      <Cart setMenu={setMenu} menu={menu} />
     </div>
   )
 }
